51b9aed493ac38f373911097108c5cb33b273027
ilabeu <mionerandom55l@gmail.com>
2025-11-10 07:18:32 -0300

    Update ModuleAcessor.java

--- src/main/java/com/example/addon/mixin/ModuleAcessor.java
+++ src/main/java/com/example/addon/mixin/ModuleAcessor.java
@@ -7,5 +7,5 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(value = Module.class, remap = false)
 public interface ModuleAccessor {
-    @Accessor("settings") Settings randomhax$getSettings();
+    @Accessor("settings") Settings example$getSettings();
 }
