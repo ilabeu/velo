0dff63fda586247b0ae33024f0d1d4395b7fefd3
ilabeu <mionerandom55l@gmail.com>
2025-11-10 07:11:00 -0300

    Update EntityVelocityUpdateS2CPacketAccessor.java

--- src/main/java/com/example/addon/mixin/EntityVelocityUpdateS2CPacketAccessor.java
+++ src/main/java/com/example/addon/mixin/EntityVelocityUpdateS2CPacketAccessor.java
@@ -1,1 +1,12 @@
+package com.example.addon.mixin;
 
+import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(EntityVelocityUpdateS2CPacket.class)
+public interface EntityVelocityUpdateS2CPacketAccessor {
+    @Accessor("velocityX") void setVelocityX(int x);
+    @Accessor("velocityY") void setVelocityY(int y);
+    @Accessor("velocityZ") void setVelocityZ(int z);
+}
