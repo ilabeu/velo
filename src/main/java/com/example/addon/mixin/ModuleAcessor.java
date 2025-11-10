db2b633be3e642c74ce33505e23c988eadd59064
ilabeu <mionerandom55l@gmail.com>
2025-11-10 07:10:22 -0300

    Update ModuleAcessor.java

--- src/main/java/com/example/addon/mixin/ModuleAcessor.java
+++ src/main/java/com/example/addon/mixin/ModuleAcessor.java
@@ -1,1 +1,11 @@
+package com.example.addon.mixin;
 
+import meteordevelopment.meteorclient.settings.Settings;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(value = Module.class, remap = false)
+public interface ModuleAccessor {
+    @Accessor("settings") Settings randomhax$getSettings();
+}
